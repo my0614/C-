8d64a3dd28996d38de05f63d48fa91dfff545a34
my0614 <aqi222@naver.com>
2020-06-28 21:28:06 +0900

    All ADD

---
+++ Nullable_example/Nullable_example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Nullable_example
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int? a = null;
+             // Console.WriteLine(a.Value); // 값이 없어서 오류
+            Console.WriteLine(a.HasValue);
+            a = 33;
+            Console.WriteLine(a != null);
+            Console.WriteLine(a.HasValue);
+        }
+    }
+}
