48b2f332649da6ff57f69e3c5296541cbb07be38
my0614 <aqi222@naver.com>
2020-10-06 19:29:48 +0900

    add : clickevent 사용하기(게임만들기)

---
+++ clickevent/clickevent/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace clickevent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        int index;
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+
+            // 
+            // pictureBox1
+            // 
+
+            this.pictureBox1.Image = global::clickevent.Properties.Resources.AVR_pinmap;
+            this.pictureBox1.Location = new System.Drawing.Point(389,179);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        void PictureArray(int count)
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox[count];
+            for(int index=0;index< pictureBox2.Length; index++)
+            {
+                this.pictureBox2[index] = new System.Windows.Forms.PictureBox();
+                ((System.ComponentModel.ISupportInitialize)(this.pictureBox2[index])).BeginInit();
+                this.pictureBox2[index].Image = global::clickevent.Properties.Resources.공유;
+                this.pictureBox2[index].Location = new System.Drawing.Point(12 + 110 *index, 12);
+                this.pictureBox2[index].Name = "pictureBox2";
+                this.pictureBox2[index].Size = new System.Drawing.Size(100, 50);
+                this.pictureBox2[index].SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+                this.pictureBox2[index].TabIndex = 1;
+                this.pictureBox2[index].TabStop = false;
+                this.Controls.Add(this.pictureBox2[index]);
+                ((System.ComponentModel.ISupportInitialize)(this.pictureBox2[index])).EndInit();
+            }
+            
+        }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox[] pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ clickevent/clickevent/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clickevent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            PictureArray(10);
+            timer1.Start(); // 타이머 시작 명령
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Left = e.X;
+            pictureBox1.Top = e.Y;
+            pictureBox2[10].Left = e.Y;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+         
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+
+        {
+            if (e.KeyCode == Keys.Left)
+                pictureBox1.Left -= 10; //왼쪽으로 이동
+            if (e.KeyCode == Keys.Right)
+                pictureBox1.Left += 10; //오른쪽으로 이동
+            if (e.KeyCode == Keys.Down)
+                pictureBox1.Top += 10; //위로 이동
+            if (e.KeyCode == Keys.Up)
+                pictureBox1.Top -= 10; //아래로 이동
+           
+          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox2[9].Top += 10;
+
+        }
+    }
+}
