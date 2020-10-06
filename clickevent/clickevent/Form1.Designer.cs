9246681a95279c587fccd676598636f639d0687f
my0614 <aqi222@naver.com>
2020-10-06 20:14:56 +0900

    add : 배열을 사용하여 랜덤으로 화면에 나타내기

--- clickevent/clickevent/Form1.Designer.cs
+++ clickevent/clickevent/Form1.Designer.cs
@@ -30,22 +30,15 @@ namespace clickevent
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox2
-            // 
-
-            // 
             // pictureBox1
             // 
-
             this.pictureBox1.Image = global::clickevent.Properties.Resources.AVR_pinmap;
-            this.pictureBox1.Location = new System.Drawing.Point(389,179);
+            this.pictureBox1.Location = new System.Drawing.Point(389, 179);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 50);
             this.pictureBox1.TabIndex = 0;
@@ -53,14 +46,14 @@ namespace clickevent
             // 
             // timer1
             // 
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            
+            this.ClientSize = new System.Drawing.Size(795, 443);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -68,7 +61,6 @@ namespace clickevent
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
-            
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
--- clickevent/clickevent/Form1.cs
+++ clickevent/clickevent/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -24,7 +25,8 @@ namespace clickevent
         {
             pictureBox1.Left = e.X;
             pictureBox1.Top = e.Y;
-            pictureBox2[10].Left = e.Y;
+        
+            
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
@@ -51,10 +53,30 @@ namespace clickevent
            
           
         }
-
+        private void pictureBox2_Click(object sender, System.EventArgs e)
+        {
+            pictureBox2[index].Visible = true;
+                
+        }
         private void timer1_Tick(object sender, EventArgs e)
         {
-            pictureBox2[9].Top += 10;
+            if(pictureBox2[0].Top == this.Height) // form1 사이즈을 넘겼을 경우
+            {
+                pictureBox2[0].Top = 0; // 0으로 초기화
+            }
+            Random rand = new Random(); // 랜덤함수 사용
+            //pictureBox2[9].Top += 10;
+            for(int index=0;index < pictureBox2.Length;index++)
+            {
+                int a = rand.Next(300); // y축
+                int b = rand.Next(800); // x축
+
+                pictureBox2[index].Top = a; // 랜덤설정
+                pictureBox2[index].Left = b; // 랜덤설정
+                //Thread.Sleep(500); // 딜레이 주기
+                
+
+            }
 
         }
     }
