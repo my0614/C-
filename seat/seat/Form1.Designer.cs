541fe52faf01bc7a57b5e9f3e5bf10d26fd35367
my0614 <aqi222@naver.com>
2020-10-20 20:11:47 +0900

    add : 원하는 값 입력하면 값만큼 좌석 랜덤 배치하기

--- seat/seat/Form1.Designer.cs
+++ seat/seat/Form1.Designer.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace seat
 {
+    
     partial class Form1
     {
         /// <summary>
@@ -30,18 +31,38 @@ namespace seat
         /// </summary>
         private void InitializeComponent()
         {
-            
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(597, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(703, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-           
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            
             this.ResumeLayout(false);
-            PanelArray(20);
+            this.PerformLayout();
 
         }
 
@@ -52,25 +73,25 @@ namespace seat
             num_panel = new System.Windows.Forms.Panel[value];
             numlabel = new System.Windows.Forms.Label[value];
 
-            for(int index = 0; index< value; index++)
+            for(int index = 0; index<value; index++)
             {
                 this.num_panel[index] = new System.Windows.Forms.Panel();
                 this.numlabel[index] = new System.Windows.Forms.Label();
                 this.num_panel[index].SuspendLayout();
                 this.num_panel[index].BackColor = System.Drawing.SystemColors.ScrollBar;
                 this.num_panel[index].Controls.Add(this.numlabel[index]);
-                this.num_panel[index].Location = new System.Drawing.Point(12+ 140 *(index%4), 25 + 120* (index / 4));
+                this.num_panel[index].Location = new System.Drawing.Point(12+ 125 *(index%4), 25 + 80 * (index / 4));
                 this.num_panel[index].Name = "num_panel";
                 this.num_panel[index].Size = new System.Drawing.Size(110, 60);
                 this.num_panel[index].TabIndex = 0;
                 this.num_panel[index].Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
                 this.numlabel[index].AutoSize = true;
-                this.numlabel[index].Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+                this.numlabel[index].Font = new System.Drawing.Font("굴림", 12.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
                 this.numlabel[index].Location = new System.Drawing.Point(41, 27);
                 this.numlabel[index].Name = "numlabel";
                 this.numlabel[index].Size = new System.Drawing.Size(40, 18);
                 this.numlabel[index].TabIndex = 0;
-                this.numlabel[index].Text = "1번";
+                this.numlabel[index].Text = ""+(1+index);
                 this.numlabel[index].Click += new System.EventHandler(this.label1_Click);
                 this.Controls.Add(this.num_panel[index]);
                 this.num_panel[index].ResumeLayout(false);
@@ -80,6 +101,8 @@ namespace seat
         }
         private System.Windows.Forms.Panel[] num_panel;
         private System.Windows.Forms.Label[] numlabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- seat/seat/Form1.cs
+++ seat/seat/Form1.cs
@@ -15,15 +15,48 @@ namespace seat
         public Form1()
         {
             InitializeComponent();
+            //PanelArray(20);
+            //RandomSeat();
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
+      
+        private void RandomSeat()
+        {
+            Random rand = new Random();
 
+            for(int i = 0; i < numlabel.Length; i++)
+            {
+                int k =  rand.Next(0, numlabel.Length);
+                string a = this.numlabel[i].Text;
+                this.numlabel[i].Text = this.numlabel[k].Text;
+                this.numlabel[k].Text = a;
+
+            }
+
+        }
         private void label1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int temp = 0;
+            try
+            {
+                temp = int.Parse(textBox1.Text);
+            }catch
+            {
+                MessageBox.Show("숫자만 입력하세요.");
+            }
+
+            PanelArray(temp);
+            RandomSeat();
+
 
         }
     }
