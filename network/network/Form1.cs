40d49c42c4e6d5e6e1b1f21d06e9a8acc08581ce
my0614 <aqi222@naver.com>
2020-10-27 19:20:39 +0900

    add : html관련함수사용

--- network/network/Form1.cs
+++ network/network/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -30,7 +31,23 @@ namespace network
             Stream docStream = this.webBrowser1.DocumentStream;
             StreamReader streamReader = new StreamReader(docStream, System.Text.Encoding.GetEncoding(webBrowser1.Document.Encoding));
             string strHtml = streamReader.ReadToEnd();
-         
+            HtmlNode parseNode = doc.DocumentNode.
+                 SelectSingleNode("//div[@class='box_type_l']").
+                 SelectSingleNode("//table[@class='type_2']");
+            List<List<string>> parsedTbl = parseNode
+                                .Descendants("tr")
+                                .Skip(1) //To Skip Table Header Row
+                                .Where(tr => tr.Elements("td").Count() > 1)
+                                .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
+                                .ToList();
+
+            List<List<string>> parsedTbl2 = parseNode
+                               .Descendants("tr")
+                               .Skip(1) //To Skip Table Header Row
+                               .Where(tr => tr.Elements("td").Count() > 1)
+                               .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
+                               .ToList();
+
         }
     }
 }
