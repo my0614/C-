1823dac392a7b0abe5951297e69a8e014201d7cb
minyoung <aqi222@naver.com>
2021-03-19 12:02:43 +0900

    args인자를 가지고 print하기

---
+++ Test_code/Test_code/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Test_code
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int lengh = int.Parse(args[0]); #int.Parse -> 자료형변환
+            for (int i = 1; i <= lengh; i++)
+            {
+                Console.WriteLine("hello, {0}", args[i]);
+            }
+            
+        }
+    }
+}
