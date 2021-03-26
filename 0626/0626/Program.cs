83c7980765f3aa1b657cfb8d5873f2097dec2aa2
minyoung <aqi222@naver.com>
2021-03-26 11:32:32 +0900

    0326 매개변수 개수에 따라 정렬

---
+++ 0626/0626/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _0626
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            static void func(int a, int b)
+            {
+                int[] array = new int[] { a, b };
+                
+                Array.Sort(array);
+                for(int i=0;i<2;i++)
+                {
+                    Console.Write(array[i]);
+                }
+                Console.WriteLine();
+            }
+            static void func1(int a, int b,int c)
+            {
+                int[] array = new int[] { a, b,c };
+                Array.Sort(array);
+                for (int i = 2; i >=0 ; i--)
+                {
+                    Console.Write(array[i]);
+                }
+                Console.WriteLine();
+            }
+            static void func2(int a, int b,int c, int d)
+            {
+                int[] array = new int[] { a, b ,c,d};
+
+                Array.Sort(array);
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.Write(array[i]);
+                    
+                }
+                Console.WriteLine();
+            }
+
+            func(3, 6);
+            func1(9, 6, 3);
+            func2(1, 3, 4, 8);
+            
+
+        }
+    }
+}
