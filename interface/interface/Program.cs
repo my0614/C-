eaf24da4bf4e833e3c5011fc1a9b281b4f8fc652
minyoung <aqi222@naver.com>
2021-04-23 09:55:39 +0900

    file Log 찍기

---
+++ interface/interface/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Interface
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+    }
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine(
+                "{0} {1}", DateTime.Now.ToLocalTime(), message);
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        private StreamWriter writer;
+        public FileLogger(string path)
+        {
+            writer = File.CreateText(path);
+            writer.AutoFlush = true;
+        }
+        public void WriteLog(string message)
+        {
+            writer.WriteLine("{0} {1}", DateTime.Now.ToShortDateString(), message);
+        }
+
+    }
+    class ClimateMonitor
+    {
+        private ILogger logger;
+        public ClimateMonitor(ILogger logger)
+        {
+            this.logger = logger;
+        }
+        public void start()
+        {
+            while (true)
+            {
+                Console.Write("온도를 입력하시오 : ");
+                string temperature = Console.ReadLine();
+                if (temperature == "")
+                    break;
+                logger.WriteLog("현재 온도 :" + temperature);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClimateMonitor moniter = new ClimateMonitor(new FileLogger("MyLog.txt"));
+            moniter.start();
+        }
+
+    }
+}
