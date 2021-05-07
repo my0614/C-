4f62eec4a516f4f782698c3b9d3174041c524eab
minyoung <aqi222@naver.com>
2021-05-07 10:46:44 +0900

    하노이탑 파일에 저장

--- interface/interface/Program.cs
+++ interface/interface/Program.cs
@@ -12,28 +12,30 @@ namespace Interface
     }
     class ConsoleLogger : ILogger
     {
-        public void WriteLog(string form,int n, char from, char to)
+        public void WriteLog(string form, int n, char from, char to)
         {
-            Console.WriteLine( "{0}\t{1}\t{2}\t", n,from, to);
+            Console.WriteLine("{0}\t{1}\t{2}\t", n, from, to);
         }
     }
 
     class FileLogger : ILogger
     {
         private StreamWriter writer;
+        
         public FileLogger(string path)
         {
             writer = File.CreateText(path);
             writer.AutoFlush = true;
         }
-        public void WriteLog(string form,int n, char from, char to)
+        public void WriteLog(string form, int n, char from, char to)
         {
-            writer.WriteLine("{0}\t{1}\t{2}\t",n,from, to);
+            writer.WriteLine("{0}\t{1}\t{2}\t", n, from, to);
         }
 
     }
     class ClimateMonitor
     {
+        int cnt=0;
         private ILogger logger;
         public ClimateMonitor(ILogger logger)
         {
@@ -44,17 +46,22 @@ namespace Interface
             
             if (n == 1)
             {
-                Console.WriteLine("{0}\t{1}\t{2}", n,from, to);
-                logger.WriteLog("{0}\t{1}\t{2}",n, from, to);
+                Console.WriteLine("{0}\t{1}\t{2}", n, from, to);
+                logger.WriteLog("{0}\t{1}\t{2}", n, from, to);
+            
             }
             else
             {
-                hanoi_tower(n - 1, from, to, tmp);
+                hanoi_tower(n - 1, from, tmp,to);
                 Console.WriteLine("{0}\t{1}\t{2}", n, from, to);
-                hanoi_tower(n - 1, tmp, from, to);
-                logger.WriteLog("{0}\t{1}\t{2}",n, from, to);
+                hanoi_tower(n - 1, tmp, to, from);
+                logger.WriteLog("{0}\t{1}\t{2}", n, from, to);
+            
             }
+            
+            
         }
+     
 
     }
     class Program
@@ -62,13 +69,12 @@ namespace Interface
         static void Main(string[] args)
         {
             ClimateMonitor moniter = new ClimateMonitor(new FileLogger("Hanoi.txt"));
-            int i;
-            for (i = 1; i < 13; i++)
-            {
-                Console.WriteLine("{0}번",i);
-                moniter.hanoi_tower(i, 'A', 'C', 'B');
-            }
-          
+   
+            for(int i=1;i<13;i++)
+                moniter.hanoi_tower(i, 'A', 'B', 'C');
+            
+            
+
         }
 
     }
