90ee9bc70d119b95003178a7b49fa8c9b6dabeca
minyoung <aqi222@naver.com>
2021-05-14 09:20:09 +0900

    lamda 표현식으로 문자열 합치기

---
+++ lamda/lamda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lamda
+{
+    class Program
+    {
+        delegate string Concat(string[] args);
+        static void Main(string[] args)
+        {
+            Concat concat = 
+            (arr) =>
+            {
+                string result = "";
+                foreach (string s in arr)
+                    result += s;
+                return result;
+            };
+
+            Console.WriteLine("{0}", concat(args));
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
