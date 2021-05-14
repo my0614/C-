010f8be2df043775e1c74e8dcadbc14e576ec98a
minyoung <aqi222@naver.com>
2021-05-14 10:53:03 +0900

    action을 사용하여 연산하기

---
+++ action/action/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace action
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Action act1 = () => Console.WriteLine("Action()");
+            act1();
+
+            int result = 0;
+
+            Action<int> act2 = (x) => result = x * x;
+            act2(3);
+            Console.WriteLine("result = {0}", result);
+
+            Action<double, double> act3 = (x, y) =>
+            {
+                double pi = x / y;
+                Console.WriteLine("Action(x/y){0},{1} : {2}", x, y, pi);
+            };
+            act3(22.0, 7.0);
+
+        }
+    }
+}
